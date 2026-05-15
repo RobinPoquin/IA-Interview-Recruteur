0ea3ee91f24444e09f821db4215cde3eb5d1b92f
unknown <robpoquin@gmail.com>
2026-05-15 20:47:21 +0200

    Préparation fichiers du projet

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Mona_Sans, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: "Autant d'interview avant l'entretien",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("dark", "font-sans", geist.variable)}>
+      <body
+          className={`${monaSans.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
